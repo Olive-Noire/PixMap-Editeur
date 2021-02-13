d62d459ac097faa774a2fded4f37d4acc5b05be5
Olive <69757487+OliveNoir@users.noreply.github.com>
2021-02-13 23:20:39 +0100

    Delete Sources directory

--- Sources/Externals/Class/Pixel.cpp
+++
@@ -1,9 +0,0 @@
-#include "../../Headers/Class/Pixel.hpp"
-
-Pixel::Pixel(int r = NULL,int g = NULL, int b = NULL) {
-
-    color.red = r;
-    color.green = g;
-    color.blue = b;
-
-}
--- Sources/Headers/Functions/Events.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef DEF_FUNCTIONS_EVENTS_HPP
-#define DEF_FUNCTIONS_EVENTS_HPP
-
-#include <SDL.h>
-
-void UpdateKeys(SDL_Event event);
-void UpdateMouse(SDL_Event event);
-
-#endif //DEF_FUNCTIONS_EVENTS_HPP
